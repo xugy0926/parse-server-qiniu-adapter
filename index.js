2bf7aa798d82de0ae9941a282394d78ab76b592d
xugy0926 <young_xu@qq.com>
2018-12-07 12:43:16 +0800

    use class style

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // Stores Parse files in Qiniu.
 
 const qiniu = require('qiniu')
-const optionsFromArguments = require('./lib/optionsFromArguments');
+const optionsFromArguments = require('./lib/optionsFromArguments')
 
 const zone = {
   'z0': qiniu.zone.Zone_z0, // 华东
@@ -12,69 +12,72 @@ const zone = {
   'na0': qiniu.zone.Zone_na0 // 北美
 }
 
-function QiniuAdapter() {
-  const options = optionsFromArguments(arguments)
-  this._region = options.region
-  this._bucket = options.bucket
-  this._baseUrl = options.baseUrl
-  this._accessKey = options.accessKey
-  this._secretKey = options.secretKey
-  this._privateUrl = options.privateUrl
-  this._mac = new qiniu.auth.digest.Mac(this._accessKey, this._secretKey)
-  this._config = new qiniu.conf.Config()
-  this._config.zone = zone[this._region]
+class QiniuAdapter {
+  constructor(args) {
+    const options = optionsFromArguments(args)
+    this._region = options.region
+    this._bucket = options.bucket
+    this._baseUrl = options.baseUrl
+    this._accessKey = options.accessKey
+    this._secretKey = options.secretKey
+    this._privateUrl = options.privateUrl
+    this._mac = new qiniu.auth.digest.Mac(this._accessKey, this._secretKey)
+    this._config = new qiniu.conf.Config()
+    this._config.zone = zone[this._region]
 
-  this._formUploader = new qiniu.form_up.FormUploader(this._config)
-  this._putExtra = new qiniu.form_up.PutExtra()
-  this._bucketManager = new qiniu.rs.BucketManager(this._mac, this._config);
-}
+    this._formUploader = new qiniu.form_up.FormUploader(this._config)
+    this._putExtra = new qiniu.form_up.PutExtra()
+    this._bucketManager = new qiniu.rs.BucketManager(this._mac, this._config)
+  }
 
-QiniuAdapter.prototype.uploadToken = function() {
-  return this._putPolicy.uploadToken(this._mac)
-}
+  uploadToken() {
+    const putPolicy = new qiniu.rs.PutPolicy({ scope: this._bucket })
+    return putPolicy.uploadToken(this._mac)
+  }
 
-QiniuAdapter.prototype.createFile = function(filename, data, contentType) {
-  return new Promise((resolve, reject) => {
-    this._formUploader.put(this.uploadToken(), filename, data, this._putExtra, function(
-      respErr,
-      respBody,
-      respInfo
-    ) {
-      if (respErr) {
-        reject(respErr)
-        return
-      }
-      if (respInfo.statusCode == 200) {
-        resolve(respBody)
-        return
-      } else {
-        reject(respBody)
-      }
+  createFile(filename, data, contentType) {
+    return new Promise((resolve, reject) => {
+      this._formUploader.put(this.uploadToken(), filename, data, this._putExtra, function(
+        respErr,
+        respBody,
+        respInfo
+      ) {
+        if (respErr) {
+          reject(respErr)
+          return
+        }
+        if (respInfo.statusCode == 200) {
+          resolve(respBody)
+          return
+        } else {
+          reject(respBody)
+        }
+      })
     })
-  })
-}
+  }
 
-QiniuAdapter.prototype.deleteFile = function(filename) {
-  return new Promise((resolve, reject) => {
-    this._bucketManager.delete(this._bucket, filename, function(err, respBody, respInfo) {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(respBody)
-      }
+  deleteFile(filename) {
+    return new Promise((resolve, reject) => {
+      this._bucketManager.delete(this._bucket, filename, function(err, respBody, respInfo) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(respBody)
+        }
+      })
     })
-  })
-}
+  }
 
-QiniuAdapter.prototype.getFileData = function(filename) {
-  return new Promise((resolve) => {
-    //TODO
-    resolve()
-  })
-}
+  getFileData(filename) {
+    return new Promise(resolve => {
+      //TODO
+      resolve()
+    })
+  }
 
-QiniuAdapter.prototype.getFileLocation = function(config, filename) {
-  return `${this._privateUrl}/${filename}`;
+  getFileLocation(config, filename) {
+    return `${this._privateUrl}/${filename}`
+  }
 }
 
 module.exports = QiniuAdapter
